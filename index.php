8f43f7149224688b49f360a1a3da9e5a08aa9bfe
Paolacocc <coccettipaola@gmail.com>
2023-05-05 15:25:37 +0200

    Add style

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php-strong-password</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   <link rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+
+ <div class="container mt-4">
+    <form>
+  <div class="mb-3">
+    <label for="password" class="form-label text-uppercase fs-4">Inserisci la lunghezza della password che vuoi generare</label>
+    <input type="text" class="form-control" id="password" aria-describedby="emailHelp">
+    
+  </div>
+  
+  
+  <button type="submit" class="btn btn-dark">Submit</button>
+</form>
+
+ </div>
+    
+ </body>
+ </html>
